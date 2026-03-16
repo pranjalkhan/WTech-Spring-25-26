1fa7998aa334b346837b67d1d7a8653801fa2c1d
pranjalkhan <abraradnanpranjal31@gmail.com>
2026-03-16 19:28:53 +0600

    Function for calculate total price

--- Lab Performance_MID/JS/Validation_script.js
+++ Lab Performance_MID/JS/Validation_script.js
@@ -7,3 +7,26 @@ var totalPriceInput = document.getElementById('totalPrice');
 var errorMsg = document.getElementById('errorMsg');
 
 var couponAlertShown = false;
+
+function calculateTotal() {
+    var qty = parseFloat(quantityInput.value);
+
+    if (qty < 0) {
+        quantityInput.value = 0;
+        qty = 0;
+        errorMsg.textContent = 'Quantity cannot be less than 0.';
+    } else {
+        errorMsg.textContent = '';
+    }
+
+    if (quantityInput.value === '' || isNaN(qty)) {
+        totalPriceInput.value = 0;
+        couponAlertShown = false;
+        return 0;
+    }
+
+    var total = UNIT_PRICE * qty * DAYS;
+    totalPriceInput.value = total;
+
+    return total;
+}
