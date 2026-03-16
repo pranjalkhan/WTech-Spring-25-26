d951a40d7d8a0bad953aff858c403277f06504cc
pranjalkhan <abraradnanpranjal31@gmail.com>
2026-03-16 19:26:36 +0600

    DOM reference

--- Lab Performance_MID/JS/Validation_script.js
+++ Lab Performance_MID/JS/Validation_script.js
@@ -1,3 +1,7 @@
 //static data input
 var Unit_Price = 1000;
 var Days = 30;
+
+var quantityInput = document.getElementById('quantity');
+var totalPriceInput = document.getElementById('totalPrice');
+var errorMsg = document.getElementById('errorMsg');
